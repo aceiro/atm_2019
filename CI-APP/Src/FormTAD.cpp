b3f7e973a62b9117499054a84a300d1ed513f960
gismarb <gismar_barbosa@yahoo.com.br>
2019-06-03 16:25:56 -0300

    Removendo aquivos defasados (Estrutura desnecessária)

--- CI-APP/Src/FormTAD.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../Include/Form.hpp"
-
-using std::cout;
-using std::endl;
-
-void FormTAD() {
-  cout << endl << "Aqui sera a Onde os dados serao armazenado CI, ainda esta em construcao..." << endl;
-}
