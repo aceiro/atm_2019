04161f08feb8031b8c2ef9010af9751867cd883f
gismarb <gismar_barbosa@yahoo.com.br>
2019-06-05 16:27:57 -0300

    Iniciando criaçaão da classe Form

--- CI-APP/Include/Form.hpp
+++ CI-APP/Include/Form.hpp
@@ -2,16 +2,44 @@
 #ifndef FORM_HPP
 #define FORM_HPP
 
+// Declaração das Bibliotecas internas do C++ 
+#include <iostream>
+#include <string>
+#include <time.h>
+
 // Padrões usados para a entradas do Menu, e tudo relacionado.
 #define CREATE "Criar um novo registro de CI"
 #define SELECT "Listar/Localizar um registro de CI"
 #define UPDATE "Atualizar um registro de CI"
 #define DELETE "Apagar um registro de CI"
 
-void FormData();
-void FormCreate();
-void FormUpdate();
-void FormSelect();
-void FormDelete();
+// Uso refinado do Escopo STD
+using std::string;
+using std::time_get;
+
+
+class Form {
+  private:
+    class FormData {
+      private:
+        int idCI;
+        string senderCI;
+        string recipientCI;
+        string subjectCI;
+        time_t dateCI;
+        string messageCI;
+      
+      public:
+        void setIdCI();
+        // set
+        
+
+    };
+
+  public:
+
+};
+
+
 
 #endif
