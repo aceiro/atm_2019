e40e14156599f731be56b202a1dfa6006c9f5658
Gismar Barbosa <gismar_barbosa@yahoo.com.br>
2019-06-06 01:09:08 -0300

    Iniciando FormData - Alocando estrutura

--- CI-APP/Include/Form.hpp
+++ CI-APP/Include/Form.hpp
@@ -5,8 +5,11 @@
 // Declaração das Bibliotecas internas do C++ 
 #include <iostream>
 #include <string>
-#include <time.h>
+#include <vector>
+#include <ctime>
 
+// Tamanho padrão para armazenar a string que obtivermos do horário.
+#define DATE_SIZE 100
 // Padrões usados para a entradas do Menu, e tudo relacionado.
 #define CREATE "Criar um novo registro de CI"
 #define SELECT "Listar/Localizar um registro de CI"
@@ -15,31 +18,51 @@
 
 // Uso refinado do Escopo STD
 using std::string;
-using std::time_get;
+using std::vector;
 
-
+// Declaração da Classe Form (Formulário de manipulação de dados)
 class Form {
   private:
+    // Declaração da Classe FormData que será nossa base de dados 
     class FormData {
       private:
         int idCI;
         string senderCI;
         string recipientCI;
         string subjectCI;
-        time_t dateCI;
+        char dateCI[DATE_SIZE];
         string messageCI;
       
       public:
+        // Construtor/Destrutor da Classe FormData
+        FormData(int idCI, string senderCI, string recipientCI, string subjectCI, char dateCI[], string messageCI);
+        ~FormData();
+        // Métodos da Classe FormData
         void setIdCI();
-        // set
-        
-
+        void setSenderCI(string senderCI);
+        void setRecipientCI(string recipientCI);
+        void setSubjectCI(string subjectCI);
+        void setDateCI();
+        void setMessageCI(string menssageCI);
+        int getIdCI();
+        string getSenderCI();
+        string getRecipientCI();
+        string getSubjectCI();
+        char getDateCI();
+        string getMessageCI();
     };
+    // Tornando nossa base de dados uma estrutrua de dados do tipo Vector 
+    vector<FormData> Data;
 
   public:
-
+    // Construtor/Destrutor da Classe FormData
+    Form();
+    ~Form();
+    // Métodos da Classe Form (Basicamente farão as operações do CRUD)
+    void formCreate(string senderCI, string recipientCI, string subjectCI, string messageCI);
+    void formSelect(int idCI);
+    void formUpdate(int idCI);
+    void formDelete(int idCI);
 };
-
-
-
+  
 #endif
--- CI-APP/Src/FormData.cpp
+++ CI-APP/Src/FormData.cpp
@@ -1,9 +1,69 @@
+// Declaração das Bibliotecas internas do C++ 
 #include <iostream>
+#include <string>
+
+// Declaração das Bibliotecas internas do Projeto (declação das Classes)
 #include "../Include/Form.hpp"
 
+// Uso refinado do Escopo STD
 using std::cout;
 using std::endl;
+using std::string;
 
-void FormTAD() {
-  cout << endl << "Aqui sera a Onde os dados serao armazenado CI, ainda esta em construcao..." << endl;
+
+Form::FormData::FormData(int idCI, string senderCI, string recipientCI, string subjectCI, char dateCI[], string messageCI) : idCI(idCI), senderCI(senderCI), recipientCI(recipientCI), subjectCI(subjectCI), dateCI(dateCI[DATE_SIZE]), messageCI(messageCI) {
+
+}
+  
+Form::FormData::~FormData(){
+
+
+}
+
+void Form::FormData::setIdCI() {
+
+}
+
+void Form::FormData::setSenderCI(string senderCI) {
+
+}
+
+void Form::FormData::setRecipientCI(string recipientCI) {
+
+}
+  
+void Form::FormData::setSubjectCI(string subjectCI) {
+
+}
+
+void Form::FormData::setDateCI() {
+
+}
+  
+void Form::FormData::setMessageCI(string menssageCI) {
+
+}
+  
+int Form::FormData::getIdCI() {
+
+}
+  
+string Form::FormData::getSenderCI() {
+
+}
+  
+string Form::FormData::getRecipientCI() {
+
+}
+  
+string Form::FormData::getSubjectCI() {
+
+}
+  
+char Form::FormData::getDateCI() {
+
+}
+  
+string Form::FormData::getMessageCI() {
+
 }
