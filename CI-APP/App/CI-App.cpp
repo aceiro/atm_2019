bf78cb6aa8b6a9111408c5140c8a2b9ae443c75d
Gismar Barbosa <gismar_barbosa@yahoo.com.br>
2019-06-22 10:19:04 -0300

    Removendo comentários desnecessários

--- CI-APP/App/CI-App.cpp
+++ CI-APP/App/CI-App.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include <locale.h> // Biblioteca interna do C++ para usar padrões de linguagem de acordo com o idioma
-//#include <iomanip>
 
 // Declaração das Bibliotecas internas do Projeto (declação das Classes)
 #include "../Include/Components.hpp" // Classe Funções (Métodos) auxiliares (Pause, Limpa tela, etc...)
