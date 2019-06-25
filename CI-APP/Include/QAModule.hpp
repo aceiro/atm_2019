170fb6c90efe89ce8f1d53b885847bbff7bf7f54
gismarb <gismar_barbosa@yahoo.com.br>
2019-06-24 22:37:41 -0300

    Desmembrano módulo de QA

--- CI-APP/Include/QAModule.hpp
+++
@@ -1,7 +0,0 @@
-// Cabeçalho obrigatório na criação / declaração do arquivo das Classes
-#ifndef QAMODULE_HPP
-#define QAMODULE_HPP
-
-void QAModule();
-
-#endif
--- CI-APP/Src/QAModule.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../Include/QAModule.hpp"
-
-using std::cout;
-using std::endl;
-
-void QAModule() {
-  cout << endl << "Aqui sera o Modulo de testes (Qualidade), ainda esta em construcao..." << endl;
-}
